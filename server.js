31981a36c25442f4eb97b3abd13fd877592c3c57
RainHinLee <1064202230@qq.com>
2019-06-17 08:11:29 +0800

    ''

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -9,6 +10,11 @@ app.get('/',function(req,res){
 	var file = path.resolve(__dirname,'./index.html');
 	res.sendFile(file);
 });
+	
+app.get('/short',function(req,res){
+	var file = path.resolve(__dirname,'./short.html');
+	res.sendFile(file);
+});	
 	
 app.listen(38080);
 
@@ -23,8 +29,19 @@ app.listen(38080);
     "年级":"小学二年级",
     "代理":"C"
 }
- 
-		
+
+
+字体： simsun
+
+			
+
+
+
+
+
+
+
+
 
  
  http://123.57.162.196:38080/timetable/mupdate/class_timelen/100/course_way/1/course_type/2/class_limit/1/lesson_num/2/teacher_uid/10008540/class_datetime/2019-05-21%2019:30/course_level/1/timetable_state/1class_timelen/100/course_way/1/course_type/1/class_limit/1/lesson_num/2/teacher_uid/10008540/class_datetime/2019-05-21%2019:30/course_level/1/timetable_state/1class_timelen/100/course_way/1/course_type/2/class_limit/1/lesson_num/2/teacher_uid/10008540/class_datetime/2019-05-21%2019:30/course_level/1/timetable_state/1
